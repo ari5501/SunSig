1d70b31beca5da8161ff9af7efc3d1600c21e07c
ari5501 <ari711.list@gmail.com>
2018-07-29 20:55:48 -0400

    First plugin.js change

--- plugin.js
+++ plugin.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+var SunCalc = require('suncalc');
+
+var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
+var sunrisePos = SunCalc.getPosition(times.sunrise, 51.5, -0.1);
+
+console.log(times);
+console.log(sunrisePos);
+//getCurrentPosition();
+
+  //heyy baby, how you doin
