491965ad3c0530fbf7a669fb3d204c0a4fe22e87
ari5501 <jakeelspas@gmail.com>
2018-07-29 22:09:33 -0400

    fixed api stuff

--- plugin.js
+++ plugin.js
@@ -1,10 +1,11 @@
 var SunCalc = require('suncalc');
+var Maps = require('@google/maps');
 
 var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
 var sunrisePos = SunCalc.getPosition(times.sunrise, 51.5, -0.1);
 
 console.log(times);
 console.log(sunrisePos);
-//getCurrentPosition();
+//console.log(navigator.geolocation);
 
 //hackity hack hack 2
